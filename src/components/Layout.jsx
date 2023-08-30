e55a1d188af28a9510c3060ac8c5e9566266a147
melisciar <84160185+melisciar@users.noreply.github.com>
2023-08-30 15:46:47 -0300

    Página de incio - mostrar datos de clientes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,38 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, NavLink } from 'react-router-dom';
 
 function Layout() {
   return (
     <div className="md:flex md:min-h-screen">
-      <div className="md:w-1/4 bg-blue-900 px-5 py-10"></div>
-      <main className="md:w-3/4 p-10 md:h-screen overflow-scroll"></main>
+      <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
+        <h2 className="text-4xl font-black text-center text-white">
+          CRM - clientes
+        </h2>
+        <nav className="mt-10">
+          <NavLink
+            className={({ isActive }) =>
+              `${
+                isActive ? 'text-blue-300' : 'text-white'
+              } text-2xl block mt-2 hover:text-blue-300 text-white`
+            }
+            to="/"
+          >
+            Clientes
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `${
+                isActive ? 'text-blue-300' : 'text-white'
+              } text-2xl block mt-2 hover:text-blue-300 text-white`
+            }
+            to="/clientes/nuevo"
+          >
+            Nuevo cliente
+          </NavLink>
+        </nav>
+      </aside>
+      <main className="md:w-3/4 p-10 md:h-screen overflow-scroll">
+        <Outlet />
+      </main>
     </div>
   );
 }
