c75a69ba247fe2c0857ddc3efb9fb0e7a428385f
melisciar <84160185+melisciar@users.noreply.github.com>
2023-08-31 10:29:52 -0300

    Creación del formulario para alta de clientes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout';
-import NuevoCliente from './pages/nuevoCliente';
+import NuevoCliente, {
+  action as nuevoClienteAction,
+} from './pages/nuevoCliente';
 import Index, { loader as clientesLoader } from './pages/index';
 
 const router = createBrowserRouter([
@@ -19,6 +21,7 @@ const router = createBrowserRouter([
       {
         path: '/clientes/nuevo',
         element: <NuevoCliente />,
+        action: nuevoClienteAction,
       },
     ],
   },
--- src/pages/nuevoCliente.jsx
+++ src/pages/nuevoCliente.jsx
@@ -1,5 +1,39 @@
+import { useNavigate, Form } from 'react-router-dom';
+import Formulario from '../components/Formulario';
+
+export function action() {
+  console.log('Submit al formulario');
+  return { ok: true };
+}
+
 const NuevoCliente = () => {
-  return <h1>Nuevo cliente</h1>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
+      <p className="mt-10">
+        Llena todos los campos para registrar un nuevo cliente
+      </p>
+      <div className="flex justify-end">
+        <button
+          className="bg-blue-800 text-white px-3 py-1 font-bold uppercase"
+          onClick={() => navigate(-1)}
+        >
+          Volver
+        </button>
+      </div>
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+        <Form method="POST">
+          <Formulario />
+          <input
+            type="submit"
+            className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
+            value="Registrar cliente"
+          />
+        </Form>
+      </div>
+    </>
+  );
 };
 
 export default NuevoCliente;
